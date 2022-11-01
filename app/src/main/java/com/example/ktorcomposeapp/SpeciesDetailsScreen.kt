837652bc2aecff5579dc12a549a528373b729359
Nadia Magazine <77028660+nadiamagazine@users.noreply.github.com>
2022-11-01 15:20:22 -0400

    add Crossfade and Error handling to Details screen

--- app/src/main/java/com/example/ktorcomposeapp/SpeciesDetailsScreen.kt
+++ app/src/main/java/com/example/ktorcomposeapp/SpeciesDetailsScreen.kt
@@ -1,42 +1,46 @@
 package com.example.ktorcomposeapp
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.ktorcomposeapp.model.SpeciesDetailedInfo
-import com.example.ktorcomposeapp.util.StatesOfDetailScreen
 import com.example.ktorcomposeapp.viewmodel.SpeciesDetailViewModel
 
 @Composable
 fun SpeciesDetailScreen(
     speciesName: String,
     viewModel: SpeciesDetailViewModel
+
 ) {
 
     val viewState = viewModel.liveData.observeAsState()
     viewModel.getSpeciesDetailedInfo(speciesName)
 
-    StatesOfDetailScreenWrapper(
-    )
-
-    viewState.value?.let {
-        SpeciesDetailedInformation(
-            speciesDetailedInfo = it.first()
-        )
+    Crossfade(targetState = viewModel) { viewModel ->
+        if (viewModel.success) {
+            viewState.value?.let {
+                SpeciesDetailedInformation(
+                    speciesDetailedInfo = it.first()
+                )
+            }
+        } else {
+            ErrorHandlingMessage()
+        }
     }
+
 }
+
 
 @Composable
 fun SpeciesDetailedInformation(
@@ -54,63 +58,63 @@ fun SpeciesDetailedInformation(
             Modifier
                 .padding(4.dp)
                 .fillMaxSize()
-            ) {
+        ) {
+            Text(
+                text = speciesDetailedInfo.speciesName,
+                style = MaterialTheme.typography.h3,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = speciesDetailedInfo.harvestType,
+                style = MaterialTheme.typography.body2,
+                fontWeight = FontWeight.Normal
+            )
+            speciesDetailedInfo.habitatImpacts?.let {
                 Text(
-                    text = speciesDetailedInfo.speciesName,
-                    style = MaterialTheme.typography.h3,
-                    fontWeight = FontWeight.Bold
-                )
-                Text(
-                    text = speciesDetailedInfo.harvestType,
-                    style = MaterialTheme.typography.body2,
-                    fontWeight = FontWeight.Normal
-                )
-                speciesDetailedInfo.habitatImpacts?.let {
-                    Text(
-                        text = it,
-                        style = MaterialTheme.typography.subtitle1,
-                        fontWeight = FontWeight.Normal
-                    )
-                }
-                Text(
-                    text = speciesDetailedInfo.source,
-                    style = MaterialTheme.typography.body2,
-                    fontWeight = FontWeight.Normal
-                )
-                Text(
-                    text = speciesDetailedInfo.biology,
-                    style = MaterialTheme.typography.h5,
-                    fontWeight = FontWeight.Normal
-                )
-                Text(
-                    text = speciesDetailedInfo.healthBenefits,
-                    style = MaterialTheme.typography.body2,
+                    text = it,
+                    style = MaterialTheme.typography.subtitle1,
                     fontWeight = FontWeight.Normal
                 )
             }
-        }
-    }
-
-@Composable
-fun StatesOfDetailScreenWrapper(
-    speciesInfo: StatesOfDetailScreen<SpeciesDetailedInfo>,
-    modifier: Modifier = Modifier,
-    loadingModifier: Modifier = Modifier
-) {
-    when(speciesInfo) {
-        is StatesOfDetailScreen.Success -> {}
-        is StatesOfDetailScreen.Error -> {
             Text(
-                text = speciesInfo.message!!,
-                color = Color.Red,
-                modifier = modifier
+                text = speciesDetailedInfo.source,
+                style = MaterialTheme.typography.body2,
+                fontWeight = FontWeight.Normal
             )
-        }
-        is StatesOfDetailScreen.Loading -> {
-            CircularProgressIndicator(
-                color = MaterialTheme.colors.primary,
-                modifier = loadingModifier
+            Text(
+                text = speciesDetailedInfo.biology,
+                style = MaterialTheme.typography.h5,
+                fontWeight = FontWeight.Normal
+            )
+            Text(
+                text = speciesDetailedInfo.healthBenefits,
+                style = MaterialTheme.typography.body2,
+                fontWeight = FontWeight.Normal
             )
         }
     }
 }
+
+//@Composable
+//fun StatesOfDetailScreenWrapper(
+//    speciesInfo: StatesOfDetailScreen<SpeciesDetailedInfo>,
+//    modifier: Modifier = Modifier,
+//    loadingModifier: Modifier = Modifier
+//) {
+//    when(speciesInfo) {
+//        is StatesOfDetailScreen.Success -> {}
+//        is StatesOfDetailScreen.Error -> {
+//            Text(
+//                text = speciesInfo.message!!,
+//                color = Color.Red,
+//                modifier = modifier
+//            )
+//        }
+//        is StatesOfDetailScreen.Loading -> {
+//            CircularProgressIndicator(
+//                color = MaterialTheme.colors.primary,
+//                modifier = loadingModifier
+//            )
+//        }
+//    }
+//}
